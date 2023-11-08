166348234e986e869146d4211b88b4d59979ebca
ising2025 <ishani.singh2025@gmail.com>
2023-11-08 13:50:41 -0500

    Update lineChart.js

--- js/lineChart.js
+++ js/lineChart.js
@@ -133,10 +133,6 @@ async function createChart(){
                         },
                     },
                     ticks: {
-                        callback: function(val, index){
-                            // Labeling of tick marks can be controlled by code and font size
-                            return index % 5 === 0 ? this.getLabelForValue(val) : '';
-                        },
                         font: {
                             size: 16
                         }
